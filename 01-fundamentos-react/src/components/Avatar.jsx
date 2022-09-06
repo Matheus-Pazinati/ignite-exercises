3694331a7b00c853a9b299153f2391ed12868fd1
Matheus Pazinati <matheuspazinati@gmail.com>
2022-09-06 15:41:41 -0300

    Aplica responsividade para dispositivos moveis

--- 01-fundamentos-react/src/components/Avatar.jsx
+++ 01-fundamentos-react/src/components/Avatar.jsx
@@ -3,7 +3,8 @@ import styles from './Avatar.module.css'
 export function Avatar({ source, hasBorder = true }) {
   return (
     <img 
-    className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
-    src={source}  />
+      className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
+      src={source}  
+    />
   )
 }
