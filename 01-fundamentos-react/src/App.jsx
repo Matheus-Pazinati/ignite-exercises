1469e8fd8f9a7f05a453565df3c5f2c13ff1f011
Matheus Pazinati <matheuspazinati@gmail.com>
2022-09-07 04:20:40 -0300

    Aplica iteracao na lista de posts para exibi-los

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -5,6 +5,54 @@ import { Aside } from "./components/Aside";
 import './global.css';
 import styles from './App.module.css';
 
+const posts = [
+  {
+    author: {
+      avatarUrl: 'https://github.com/Matheus-Pazinati.png',
+      name: 'Matheus Pazinati',
+      role: 'Web Developer'
+    },
+    publishedAt: new Date('2022-09-07 01:00'),
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋'
+      },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type: 'link',
+        content: '👉 jane.design/doctorcare'
+      },
+    ]
+  },
+
+  {
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Web Developer'
+    },
+    publishedAt: new Date('2022-09-07 03:00'),
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋'
+      },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type: 'link',
+        content: '👉 jane.design/doctorcare'
+      },
+    ]
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -12,8 +60,9 @@ export function App() {
       <div className={styles.wrapper}>
         <Aside />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return <Post />
+          })}
         </main>
       </div>
     </div>
