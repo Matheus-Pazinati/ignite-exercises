dfe19f46e339b9339a42038ea1ef4fa03f0cc9ff
Matheus Pazinati <matheuspazinati@gmail.com>
2023-02-13 13:47:14 -0300

    Cria mock do componente Link nos testes

--- unit-testing-jest/src/components/List/List.spec.tsx
+++ unit-testing-jest/src/components/List/List.spec.tsx
@@ -2,22 +2,27 @@ import { List } from "."
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
+
+jest.mock("react-router-dom", () => ({
+  Link: "Link"
+}))
+
 describe("List component test", () => {
   it("should render", () => {
     render(<List initialUsers={["Matheus", "Carol"]} />)
 
-    expect(screen.getByText("Matheus")).toBeInTheDocument
+    expect(screen.getByText(/Matheus/i)).toBeInTheDocument
   });
 
   it("should be able to add new item on the list", async () => {
-    const { getByText, getByPlaceholderText } = render(<List initialUsers={["Matheus", "Carol"]} />);
+    render(<List initialUsers={["Matheus", "Carol"]} />);
 
-    const inputElement = getByPlaceholderText('Novo usuário');
-    const addButton = getByText('Adicionar usuário');
+    const inputElement = screen.getByPlaceholderText('Novo usuário');
+    const addButton = screen.getByText('Adicionar usuário');
 
     await userEvent.type(inputElement, 'Pedro');
     await userEvent.click(addButton);
 
-    expect(getByText('Pedro')).toBeInTheDocument
+    expect(screen.getByText("Pedro")).toBeInTheDocument
   });
 });
---
+++ unit-testing-jest/src/pages/Home/Home.spec.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react"
+import { Home } from "."
+
+jest.mock("react-router-dom", () => ({
+  Link: "Link"
+}))
+
+describe("Home page", () => {
+  it("should render Home page", () => {
+    render(<Home />)
+
+    expect(screen.getByText("Home")).toBeInTheDocument
+  })
+})
