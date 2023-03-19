be6eecb9736a5c0d68a89a01781620cdd5628327
Matheus Pazinati <matheuspazinati@gmail.com>
2023-03-19 06:34:21 -0300

    Cria formulario simples de login

--- authentication-authorization/src/pages/index.tsx
+++ authentication-authorization/src/pages/index.tsx
@@ -1,9 +1,39 @@
+import { FormEvent, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+    console.log({email, password})
+    setEmail('')
+    setPassword('')
+  }
+
   return (
-    <div className={styles.container}>
-      <h1>Hello World</h1>
-    </div>
+    <form className={styles.container} onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">E-mail</label>
+        <input 
+        type="email" 
+        placeholder='xxx@email.com' 
+        id='email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value) }
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Senha</label>
+        <input 
+        type="password" 
+        id='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type='submit'>Login</button>
+    </form>
   )
 }
