d7a444e9204fecd28f273c36f2c57e1401c20ac4
Matheus Pazinati <matheuspazinati@gmail.com>
2023-03-23 05:57:15 -0300

    Intercepta resposta de token expirado para gerar um novo atraves do refresh token

--- authentication-authorization/src/lib/api.ts
+++ authentication-authorization/src/lib/api.ts
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from 'axios'
+import { parseCookies, setCookie } from 'nookies'
 
 const {'token:nextauth': token} = parseCookies()
 
@@ -9,3 +9,29 @@ export const api = axios.create({
     Authorization: `Bearer ${token}`
   }
 })
+
+api.interceptors.response.use((response) => {
+  return response
+},
+ (error) => {
+  if (error.response?.status === 401) {
+    if (error.response.data?.code === 'token.expired') {
+      const {'refreshToken:nextauth': refreshToken} = parseCookies()
+      api.post('/refresh', {
+        refreshToken
+      })
+      .then((response) => {
+        const { token } = response.data
+        setCookie(undefined, 'token:nextauth', token, {
+          maxAge: 60 * 60 *24 * 30, //1 month
+          path: '/'
+        })
+        setCookie(undefined, 'refreshToken:nextauth', response.data.refreshToken)
+        api.defaults.headers['Authorization'] = `Bearer ${token}`
+      })
+    } else {
+      //Deslogar usuário
+    }
+  }
+ }
+)
